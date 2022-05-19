fce74a769995725ee254bccd5b6363a9df467219
이지은 <tori@ryanlab.kr>
2022-05-19 18:41:45 +0900

    fix server.js

--- server.js
+++ server.js
@@ -49,10 +49,10 @@ if(process.env.NODE_ENV === 'production'){
 
 // Deployment
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
+    app.use(express.static(path.join(__dirname, "client/build")));
     app.get("*", (req, res) =>
       // res.sendFile(path.resolve(__dirname, "../client", "public", "index.html"))
-      res.sendFile(path.resolve(__dirname, "../client/build/index.html"))
+      res.sendFile(path.resolve(__dirname, "client/build/index.html"))
     );
   } else {
     app.get("/", (req, res) => {
