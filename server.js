ce85d6b084475d601bd0a3ba62b83b0b065a420a
이지은 <tori@ryanlab.kr>
2022-05-20 18:25:10 +0900

    fix package.json & client package.json & server.js

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ require('dotenv').config();
 
 // express & 포트 만들기
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json()); // json 파싱
-app.set('port', port);
 
 // mongoDB
 const uri = process.env.ATLAS_URI;
@@ -49,12 +48,17 @@ if(process.env.NODE_ENV === 'production'){
 
 // Deployment
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-} 
-  
-app.get("/", (request, response) => {
-    response.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
+    app.use(express.static(path.join(__dirname, "client/build")));
+    app.get("*", (req, res) =>
+      // res.sendFile(path.resolve(__dirname, "../client", "public", "index.html"))
+      res.sendFile(path.resolve(__dirname, "client/build/index.html"))
+    );
+  } else {
+    app.get("/", (req, res) => {
+      res.send("API IS RUNNING.");
+    });
+}
+
 
 // 서버 시작
 app.listen(port, () => {
