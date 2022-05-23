71ec28ededbff758178ab3dc03853701c39b0589
이지은 <tori@ryanlab.kr>
2022-05-23 15:20:26 +0900

    fix server.js

--- server.js
+++ server.js
@@ -36,10 +36,10 @@ app.use('/heart/', heartRouter);
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static(__dirname, 'client/build'));
+    app.use(express.static('client/build'));
   
     app.get('/*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
