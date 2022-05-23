bbbf763c09c134411d8ae1dc98a651067f40d3f0
이지은 <tori@ryanlab.kr>
2022-05-23 17:40:23 +0900

    fix setupProxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(createProxyMiddleware('/api', { 
-    target: 'http://localhost:5000', 
+    target: 'https://protected-wildwood-11173.herokuapp.com', 
     changeOrigin: true,
     pathRewrite: {
       '^/api': '' // URL ^/api -> 공백 변경
