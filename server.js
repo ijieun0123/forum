edfcb969358477b7343dbb628b9f6eed17452a10
이지은 <tori@ryanlab.kr>
2022-05-23 14:55:39 +0900

    fix package.json

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
     app.get('/*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-  }
+}
 
 // 서버 시작
 app.listen(port, () => {
