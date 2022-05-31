27596f447078b6eb1a97fc16796cab910e28ae71
이지은 <tori@ryanlab.kr>
2022-05-31 11:04:05 +0900

    fix server.js

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(express.json({limit: '50mb'})); // 파일 업로드 용량
-app.use(express.urlencoded({limit: '50mb', extended:true}));
+app.use(express.json()); // 파일 업로드 용량
+app.use(express.urlencoded({extended:false}));
+
 
 // mongoDB
 const uri = process.env.ATLAS_URI;
@@ -38,9 +39,9 @@ app.use('/api/heart', heartRouter);
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static(path.join(__dirname, 'client/build')));
-  
+
     app.get('/api/*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
