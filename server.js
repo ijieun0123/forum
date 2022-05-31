13daa249ca6af5cd1f256bb6d90fef0234361822
이지은 <tori@ryanlab.kr>
2022-05-31 14:58:40 +0900

    fix .env

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(express.json({limit:'50mb', extended:true})); // 파일 업로드 용량
+app.use(express.json({limit:'50mb'})); // 파일 업로드 용량
 app.use(express.urlencoded({limit:'50mb', extended:true}));
 /*
 app.use(express.json({limit:'50mb'})); // 파일 업로드 용량
