c3a090ca909935259b779d4a1b998cd7311528ad
이지은 <tori@ryanlab.kr>
2022-07-27 15:39:34 +0900

    fix mainText checkSchema

--- validation/validate.js
+++ validation/validate.js
@@ -12,15 +12,20 @@ module.exports = validations => {
 
         if (errors.isEmpty()) { // validation 통과
             return next();
-        } else{  // validation 불통
+        } else{  // validation 불통'
+            /*
+            const attachImageNames = req.body.attachImageNames;
+            console.log(typeof attachImageNames)
             // cloudinary 이미지 삭제
-            if(req.file) {
-                cloudinary.uploader.destroy(
-                    req.file.filename, 
-                    (err, res) => { console.log(res, err) }
-                )
+            if(attachImageNames) {
+                for(let attachImageName of attachImageNames){
+                    cloudinary.uploader.destroy(
+                        attachImageName, 
+                        (err, res) => { console.log(res, err) }
+                    )
+                }
             }
-
+            */
             res.status(400).json( 
                 errors.array()[0].msg
             );
