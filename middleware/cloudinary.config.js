7ac4841ae0c82a9c79f949d253f1f50d6c5d66be
이지은 <tori@ryanlab.kr>
2022-08-03 14:18:19 +0900

    환경변수 수정

--- middleware/cloudinary.config.js
+++ middleware/cloudinary.config.js
@@ -6,12 +6,12 @@ const {
     CLOUDINARY_HOST,
     CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET,
-} = process.env;
+} = `${process.env}`;
 
 cloudinary.config({
-    cloud_name: `${CLOUDINARY_HOST}`,
-    api_key: `${CLOUDINARY_API_KEY}`,
-    api_secret: `${CLOUDINARY_API_SECRET}`,
+    cloud_name: CLOUDINARY_HOST,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET,
 })
 
 const storage = new CloudinaryStorage({
